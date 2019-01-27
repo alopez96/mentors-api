892f68aecbd2bf410544c6a98260859ec2d296d5
Arturo <alopez96@ucsc.edu>
2019-01-26 21:09:04 -0800

    init, registpost method for register is working

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'mentors-db'
+    }
+  });
+  
+  db.select('*').from('users').then(data => {
+    console.log(data)
+  })
+  
+  const app = express();
+  
+  app.use(bodyParser.json());
+  app.use(cors());
+
+app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
